b326118b97589f9c3b36489707787479f89a6076
teamcoe-teal <tealrmpadmin@titan.co.in>
2023-01-25 09:21:01 +0530

    Bug fix on updating ip

--- CBM_BACKEND/OPC-UA-Client/plcRead/connectionurl.js
+++ CBM_BACKEND/OPC-UA-Client/plcRead/connectionurl.js
@@ -1,6 +1,6 @@
 const os = require("os");
 const ip = os.networkInterfaces();
-const url = ip.WiFi[0].address ?  ip.WiFi[0].address : ip['Loopback Pseudo-Interface 1'][1].address;
+const url = ip.WiFi ?  ip.WiFi[0].address : ip['Loopback Pseudo-Interface 1'][1].address;
 const ipAddress = url;
 
 exports.connectionURLS = ipAddress;
