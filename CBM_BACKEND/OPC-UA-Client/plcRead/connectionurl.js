585d5357f857c342747d3a2ddf739584722984ac
John Doe <johndoe@example.com>
2023-02-01 10:08:52 +0530

    ip setup in local pc

--- CBM_BACKEND/OPC-UA-Client/plcRead/connectionurl.js
+++ CBM_BACKEND/OPC-UA-Client/plcRead/connectionurl.js
@@ -1,7 +1,9 @@
 const os = require("os");
 const ip = os.networkInterfaces();
-const url = ip.WiFi ?  ip.WiFi[0].address : ip['Loopback Pseudo-Interface 1'][1].address;
+const url = ip['Wi-Fi'] ?  ip['Wi-Fi'][0].address : ip['Loopback Pseudo-Interface 1'][1].address;
 const ipAddress = url;
+
+console.log("url",ip);
 
 exports.connectionURLS = ipAddress;
 exports.baseurl2 = `https://${ipAddress}`;
