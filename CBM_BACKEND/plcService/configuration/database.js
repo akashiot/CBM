42911f6b268db07437b857f4bff4098166f0cdd1
teamcoe-teal <72795414+teamcoe-teal@users.noreply.github.com>
2023-03-25 12:48:15 +0530

    Plc service deleted

--- CBM_BACKEND/plcService/configuration/database.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize, Model, DataTypes }=require('sequelize')
-const mysql=require('mysql2')
-const sequelize = new Sequelize("cbm", "root", "password", {
-    host: "localhost",
-    dialect: "mysql",
-  });
-module.exports=sequelize
